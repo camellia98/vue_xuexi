290caae95058db6d8e55bae233a1c6aefd58d320
zhangludian <2927375411@qq.com>
2024-01-12 18:53:11 +0800

    练习案例

---
+++ vue_test/28_src_求和案例_多组件共享数据/App.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		<Count/>
+		<hr>
+		<person></person>
+	</div>
+</template>
+
+<script>
+	import Count from './components/Count'
+	import Person from './components/Person.vue'
+	
+	export default {
+		name:'App',
+		components:{Count,Person},
+		mounted() {
+			// console.log('App',this)
+		},
+	}
+</script>
